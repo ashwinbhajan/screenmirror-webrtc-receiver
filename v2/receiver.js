34e3b894641b75bc3c8f0c3bd9ea9facdcccf63f
Ashwin <ashwin@Ashwins-MacBook-Pro.local>
2026-09-19 21:43:35 +0200

    feat(debug-receiver): use supplied cast device icon

--- v2/receiver.js
+++ v2/receiver.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const RECEIVER_VERSION = "2.0.0";
-  const RECEIVER_REVISION = "corr-fragseq-20260919-normalstop2";
+  const RECEIVER_REVISION = "corr-fragseq-20260919-normalstop2-icon";
   const PROTOCOL_VERSION = 2;
   const NAMESPACE = "urn:x-cast:com.ashwinbhajan.screenmirror.cmafprobe.v2";
   const MIME_TYPE = 'video/mp4; codecs="avc1.42e01f"';
